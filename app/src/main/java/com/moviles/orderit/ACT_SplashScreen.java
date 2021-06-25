b6bb9716b980133eb03278aeef4bc6fa6489747a
R_Giron_C <ricardo.giron.colorado@gmail.com>
2021-06-25 17:30:56 -0500

    App con icono personalizado y Splash Screen

---
+++ app/src/main/java/com/moviles/orderit/ACT_SplashScreen.java
@@ -0,0 +1,39 @@
+package com.moviles.orderit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ACT_SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_act_splash_screen);
+
+        //ANIMACIONES
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView TV_Eslogan = findViewById(R.id.tv_eslogan);
+        ImageView IV_Logo = findViewById(R.id.iv_logo);
+
+        TV_Eslogan.setAnimation(animacion2);
+        IV_Logo.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(ACT_SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 4000);
+    }
+}
