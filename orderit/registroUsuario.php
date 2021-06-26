13de5ea5ece456c34787614d5611bc59c8f99ea1
AlduinDarknate <77372512+AlduinDarknate@users.noreply.github.com>
2021-06-25 19:42:34 -0500

    Add files via upload

--- orderit/registroUsuario.php
+++ orderit/registroUsuario.php
@@ -1,26 +1,26 @@
 <?PHP
 $hostname_localhost="localhost";
-$database_localhost="orderit";
+$database_localhost="db_orderit";
 $username_localhost="root";
 $password_localhost="";
 
 $json=array();
 
-if(isset($_GET["idUsuarios"]) && isset($_GET["nombre"]) && isset($_GET["apellidoPaterno"]) && isset($_GET["apellidoMaterno"]) && isset($_GET["email"]) && isset($_GET["password"])){
-    $idUsuarios=$_GET['idUsuarios'];
-    $nombre=$_GET['nombre'];
-    $apellidoPaterno=$_GET['apellidoPaterno'];
-    $apellidoMaterno=$_GET['apellidoMaterno'];
+if(isset($_GET["idUsuario"]) && isset($_GET["nombreUsuario"]) && isset($_GET["apPaterno"]) && isset($_GET["apMaterno"]) && isset($_GET["email"]) && isset($_GET["password"])){
+    $idUsuario=$_GET['idUsuario'];
+    $nombreUsuario=$_GET['nombreUsuario'];
+    $apPaterno=$_GET['apPaterno'];
+    $apMaterno=$_GET['apMaterno'];
     $email=$_GET['email'];
     $password=$_GET['password'];
 
     $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
 
-    $insert="INSERT INTO usuarios(idUsuarios, nombre, apellidoPaterno, apellidoMaterno, email, password) VALUES ('$idUsuarios','$nombre','$apellidoPaterno','$apellidoMaterno','$email','$password')";
+    $insert="INSERT INTO usuarios(idUsuario, nombreUsuario, apPaterno, apMaterno, email, password) VALUES ('$idUsuario','$nombreUsuario','$apPaterno','$apMaterno','$email','$password')";
     $resultado_insert=mysqli_query($conexion,$insert);
 
     if($resultado_insert){
-        $consulta="SELECT * FROM usuarios WHERE idUsuarios = '{$idUsuarios}'";
+        $consulta="SELECT * FROM usuarios WHERE idUsuario = '{$idUsuario}'";
         $resultado=mysqli_query($conexion,$consulta);
 
         if($registro=mysqli_fetch_array($resultado)){
@@ -30,10 +30,10 @@ if(isset($_GET["idUsuarios"]) && isset($_GET["nombre"]) && isset($_GET["apellido
         echo json_encode($json);
     }
     else{
-        $resulta["idUsuarios"]=0;
-        $resulta["nombre"]='No registra';
-        $resulta["apellidoPaterno"]='No registra';
-        $resulta["apellidoMaterno"]='No registra';
+        $resulta["idUsuario"]=0;
+        $resulta["nombreUsuario"]='No registra';
+        $resulta["apPaterno"]='No registra';
+        $resulta["apMaterno"]='No registra';
         $resulta["email"]='No registra';
         $resulta["password"]='No registra';
         $json['usuarios'][]=$resulta;
@@ -42,10 +42,10 @@ if(isset($_GET["idUsuarios"]) && isset($_GET["nombre"]) && isset($_GET["apellido
 
 }
 else{
-    $resulta["idUsuarios"]=0;
-        $resulta["nombre"]='WS No registra';
-        $resulta["apellidoPaterno"]='WS No registra';
-        $resulta["apellidoMaterno"]='WS No registra';
+    $resulta["idUsuario"]=0;
+        $resulta["nombreUsuario"]='WS No registra';
+        $resulta["apPaterno"]='WS No registra';
+        $resulta["apMaterno"]='WS No registra';
         $resulta["email"]='WS No registra';
         $resulta["password"]='WS No registra';
         $json['usuarios'][]=$resulta;
