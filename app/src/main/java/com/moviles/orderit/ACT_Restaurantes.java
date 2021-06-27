920f170c045a8774879b2d1129e0d3165af3c3f4
R_Giron_C <ricardo.giron.colorado@gmail.com>
2021-06-27 16:44:04 -0500

    Añadida actividad de prueba para probar Login

---
+++ app/src/main/java/com/moviles/orderit/ACT_Restaurantes.java
@@ -0,0 +1,14 @@
+package com.moviles.orderit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ACT_Restaurantes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_act_restaurantes);
+    }
+}
