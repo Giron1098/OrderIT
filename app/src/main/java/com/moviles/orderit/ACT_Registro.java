9966567d91d7c465bfca7e53df4f98d714e3654c
AlduinDarknate <erick_98omar@hotmail.com>
2021-06-28 15:02:09 -0500

    registro

--- app/src/main/java/com/moviles/orderit/ACT_Registro.java
+++ app/src/main/java/com/moviles/orderit/ACT_Registro.java
@@ -3,12 +3,73 @@ package com.moviles.orderit;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ACT_Registro extends AppCompatActivity {
+
+    EditText et_nombre,et_apPaterno,et_apMaterno,et_email,et_password;
+    Button btn_registroUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lay_act_registro);
+
+        et_nombre=findViewById(R.id.ET_NombreRegister);
+        et_apPaterno=findViewById(R.id.ET_ApPaternoRegister);
+        et_apMaterno=findViewById(R.id.ET_ApMaternoRegister);
+        et_email=findViewById(R.id.ET_EmailRegister);
+        et_password=findViewById(R.id.ET_PasswordRegister);
+        btn_registroUsuario=findViewById(R.id.BTN_SubmitRegister);
+
+        btn_registroUsuario.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ejecutarServicio("http://192.168.1.5/orderit/registroUsuario.php");
+            }
+        });
+    }
+
+    private void ejecutarServicio(String URL){
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "Registro Exitoso", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros=new HashMap<String, String>();
+                parametros.put("nombreUsuario",et_nombre.getText().toString());
+                parametros.put("apPaterno",et_apPaterno.getText().toString());
+                parametros.put("apMaterno",et_apMaterno.getText().toString());
+                parametros.put("email",et_email.getText().toString());
+                parametros.put("password",et_password.getText().toString());
+
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
--- orderit/registroUsuario.php
+++ orderit/registroUsuario.php
@@ -1,54 +1,14 @@
 <?PHP
-$hostname_localhost="localhost";
-$database_localhost="db_orderit";
-$username_localhost="root";
-$password_localhost="";
+include 'conexion.php';
+$idUsuario=$_POST['idUsuario'];
+$nombreUsuario=$_POST['nombreUsuario'];
+$apPaterno=$_POST['apPaterno'];
+$apMaterno=$_POST['apMaterno'];
+$email=$_POST['email'];
+$password=$_POST['password'];
 
-$json=array();
+$consulta="INSERT INTO usuarios VALUES('".null."','".$nombreUsuario."','".$apPaterno."','".$apMaterno."','".$email."','".$password."')";
 
-if(isset($_GET["idUsuario"]) && isset($_GET["nombreUsuario"]) && isset($_GET["apPaterno"]) && isset($_GET["apMaterno"]) && isset($_GET["email"]) && isset($_GET["password"])){
-    $idUsuario=$_GET['idUsuario'];
-    $nombreUsuario=$_GET['nombreUsuario'];
-    $apPaterno=$_GET['apPaterno'];
-    $apMaterno=$_GET['apMaterno'];
-    $email=$_GET['email'];
-    $password=$_GET['password'];
-
-    $conexion=mysqli_connect($hostname_localhost,$username_localhost,$password_localhost,$database_localhost);
-
-    $insert="INSERT INTO usuarios(idUsuario, nombreUsuario, apPaterno, apMaterno, email, password) VALUES ('$idUsuario','$nombreUsuario','$apPaterno','$apMaterno','$email','$password')";
-    $resultado_insert=mysqli_query($conexion,$insert);
-
-    if($resultado_insert){
-        $consulta="SELECT * FROM usuarios WHERE idUsuario = '{$idUsuario}'";
-        $resultado=mysqli_query($conexion,$consulta);
-
-        if($registro=mysqli_fetch_array($resultado)){
-            $json['usuarios'][]=$registro;
-        }
-        mysqli_close($conexion);
-        echo json_encode($json);
-    }
-    else{
-        $resulta["idUsuario"]=0;
-        $resulta["nombreUsuario"]='No registra';
-        $resulta["apPaterno"]='No registra';
-        $resulta["apMaterno"]='No registra';
-        $resulta["email"]='No registra';
-        $resulta["password"]='No registra';
-        $json['usuarios'][]=$resulta;
-        echo json_encode($json);
-    }
-
-}
-else{
-    $resulta["idUsuario"]=0;
-        $resulta["nombreUsuario"]='WS No registra';
-        $resulta["apPaterno"]='WS No registra';
-        $resulta["apMaterno"]='WS No registra';
-        $resulta["email"]='WS No registra';
-        $resulta["password"]='WS No registra';
-        $json['usuarios'][]=$resulta;
-        echo json_encode($json);
-}
+mysqli_query($conexion,$consulta) or die (mysqli_error());
+mysqli_close($conexion);
 ?>
