6bf698d7c366bad952fba819ba6031e6b5263dc6
R_Giron_C <ricardo.giron.colorado@gmail.com>
2021-06-29 18:51:53 -0500

    Modificación Web Service de consultar restaurantes

--- orderit/consultaRestaurantes.php
+++ orderit/consultaRestaurantes.php
@@ -1,13 +1,29 @@
 <?php
 include 'conexion.php';
 
-$consulta= "SELECT * FROM restaurante";
-$resultado = $conexion -> query($consulta);
+$consulta= "SELECT idRestaurante, nombreRest, direccion, horario, tiempoEstimado, costoEntrega FROM restaurante";
+$resultado = $conexion -> prepare($consulta);
 
-while($fila=$resultado -> fetch_array()){
-    $producto[] = array_map('utf8_encode', $fila);
+$resultado->execute();
+
+$resultado->bind_result($idRestaurante, $nombreRest, $direccion, $horario, $tiempoEstimado, $costoEntrega);
+
+$restaurantes = array();
+
+while($resultado->fetch()){
+	
+	$temp = array();
+	$temp['idRestaurante'] = $idRestaurante;
+	$temp['nombreRest'] = $nombreRest;
+	$temp['direccion'] = $direccion;
+	$temp['horario'] = $horario;
+	$temp['tiempoEstimado'] = $tiempoEstimado;
+	$temp['costoEntrega'] = $costoEntrega;
+	
+	array_push($restaurantes, $temp);
+    
 }
 
-echo json_encode($producto);
-$resultado->close();
+echo json_encode($restaurantes);
+
 ?>
