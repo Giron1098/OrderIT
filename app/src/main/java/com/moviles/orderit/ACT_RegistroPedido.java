6993e66f3d191c25eb48b035ac11d82a2e086d70
david <bengala260@gmail.com>
2021-07-01 15:36:17 -0500

    pedidos1

---
+++ app/src/main/java/com/moviles/orderit/ACT_RegistroPedido.java
@@ -0,0 +1,14 @@
+package com.moviles.orderit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ACT_RegistroPedido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_registro_pedido);
+    }
+}
